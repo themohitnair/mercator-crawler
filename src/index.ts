b8e88b6aad8b22d32b094a27c173234b27212ec0
Mohit <themohitnair@protonmail.com>
2024-10-27 20:53:31 +0530

    Added not-found and error handlers + url validation

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,28 @@ import { Hono } from 'hono'
 
 const app = new Hono<{ Bindings: CloudflareBindings }>()
 
-app.get('/', (c) => {
+app.get('/', async (c) => {
     return c.json({
         message: "Hello from mercator-scraper!"
     })
+})
+
+app.get('/scrape', async (c) => {
+    const url: string | URL = c.req.query('url') as string
+    try {
+        new URL(url)
+    } catch {
+        throw Error("URL is invalid!")
+    }
+})
+
+app.onError((err, c) => {
+    console.error(`${err}`)
+    return c.text("An Error occurred.", 500)
+})
+
+app.notFound((c) => {
+    return c.text('Endpoint not found', 404)
 })
 
 export default app
