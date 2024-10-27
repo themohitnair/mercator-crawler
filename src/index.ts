fdfed6dce3a7929ff0fc25d0a52a6662733537d3
Mohit <themohitnair@protonmail.com>
2024-10-27 21:57:49 +0530

    Added OpenAPI compliant scrape endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import { fromHono } from "chanfana";
 import { Hono } from "hono";
-import { TaskCreate } from "./endpoints/taskCreate";
-import { TaskDelete } from "./endpoints/taskDelete";
-import { TaskFetch } from "./endpoints/taskFetch";
-import { TaskList } from "./endpoints/taskList";
+import { Scrape } from "endpoints/scrape";
 
 // Start a Hono app
 const app = new Hono();
@@ -14,10 +11,7 @@ const openapi = fromHono(app, {
 });
 
 // Register OpenAPI endpoints
-openapi.get("/api/tasks", TaskList);
-openapi.post("/api/tasks", TaskCreate);
-openapi.get("/api/tasks/:taskSlug", TaskFetch);
-openapi.delete("/api/tasks/:taskSlug", TaskDelete);
+openapi.get("/api/scrape", Scrape)
 
 // Export the Hono app
 export default app;
--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,11 @@
-import { DateTime, Str } from "chanfana";
 import { z } from "zod";
 
-export const Task = z.object({
-	name: Str({ example: "lorem" }),
-	slug: Str(),
-	description: Str({ required: false }),
-	completed: z.boolean().default(false),
-	due_date: DateTime(),
+export const URLReq = z.object({
+    url: z.string().url(),
+})
+
+export const ScrapeRes = z.object({
+    url: z.string().url(),
+    success: z.boolean(),
+    data: z.record(z.string()),
 });
