3394c3fcefd5d9aaee7303932d1aaa5c75776451
Mohit <themohitnair@protonmail.com>
2024-10-28 20:56:47 +0530

    endpoint name change

---
+++ src/endpoints/crawl.ts
@@ -0,0 +1,50 @@
+import { OpenAPIRoute } from "chanfana";
+import { ScrapeRes, URLReq } from "../types";
+
+export class Scrape extends OpenAPIRoute {
+    schema = {
+        tags: ["Routes"],
+        summary: "Scrapes a given website (query param URL)",
+        request: {
+            query: URLReq
+        },
+        responses: {
+            "200": {
+                description: "Returns the scraped data",
+                content: {
+                    "application/json": {
+                        schema: ScrapeRes,
+                    },
+                },
+            },
+            "400": {
+                description: "Invalid URL or request data",
+            },
+            "500": {
+                description: "Internal server error",
+            },
+        },
+    };
+
+    async handle(c) {
+        const data = await this.getValidatedData<typeof this.schema>();
+
+        const url = data.query.url
+
+        try {
+            // const scrapeResult = await this.runCrawler(url);
+
+            return c.json({
+                url,
+                success: true,
+                // data: scrapeResult,
+            });
+        } catch (error) {
+            console.error("Error scraping website:", error);
+            return c.status(500).json({
+                success: false,
+                message: "Failed to scrape the website.",
+            });
+        }
+    }
+}
