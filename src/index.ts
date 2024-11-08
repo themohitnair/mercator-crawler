5385793024d23e3f29650aff9ea679c40c1cba18
Mohit <themohitnair@protonmail.com>
2024-11-08 17:11:03 +0530

    I hate typescript semicolons

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,13 @@
-import { fromHono } from "chanfana";
-import { Hono } from "hono";
-import { Crawl } from "endpoints/crawl";
+import { fromHono } from "chanfana"
+import { Hono } from "hono"
+import { Crawl } from "endpoints/crawl"
 
-// Start a Hono app
-const app = new Hono();
+const app = new Hono()
 
-// Setup OpenAPI registry
 const openapi = fromHono(app, {
 	docs_url: "/",
 });
 
-// Register OpenAPI endpoints
 openapi.get("/api/crawl", Crawl)
 
-// Export the Hono app
-export default app;
+export default app
